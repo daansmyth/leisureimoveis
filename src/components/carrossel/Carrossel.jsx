2984a52182d1d8493965e284c7e03720e5a057c1
Danilo da Cruz <cruz.delfino@outlook.com>
2024-07-12 12:54:41 -0300

    ajustes nas imagens que estavam quebradas.

--- src/components/carrossel/Carrossel.jsx
+++ src/components/carrossel/Carrossel.jsx
@@ -1,14 +1,18 @@
 import './Carrossel.css'
+import anuncio1 from '../../assets/banner/anuncio1.png'
+import anuncio2 from '../../assets/banner/anuncio2.png'
+
+
 const Carrossel = () => {
     return (
         <div className="carrossel">
             <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
                 <div className="carousel-inner">
                     <div className="carousel-item active" data-bs-interval="5000">
-                        <img src='/src/assets/banner/anuncio1.png' className="d-block w-100" alt="Anuncio 01" />
+                        <img src={anuncio1} className="d-block w-100" alt="Anuncio 01" />
                     </div>
                     <div className="carousel-item active" data-bs-interval="5000">
-                        <img src='/src/assets/banner/anuncio2.png' className="d-block w-100" alt="Anuncio 02" />
+                        <img src={anuncio2} className="d-block w-100" alt="Anuncio 02" />
                     </div>
                 </div>
                 <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
