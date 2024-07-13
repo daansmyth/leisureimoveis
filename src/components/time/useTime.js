b78261d67ab8ef2943fa94fd8200f01c53ac39e0
Danilo da Cruz <cruz.delfino@outlook.com>
2024-07-12 22:51:57 -0300

    Ajuste -Alterado local do Time.json

--- src/components/time/useTime.js
+++ src/components/time/useTime.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 
 export const useTime = () => {
     const [time, setTime] = useState([]);
-    const url = './Time.json'
+    const url = './data/Time.json'
 
     useEffect(() => {
 
